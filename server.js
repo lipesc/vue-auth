08ca8b7c8ab9035775ca7cd9d69fdc4aa8b288e6
lipesc <felipedrhouse@hotmail.com>
2025-01-31 01:08:05 -0300

    Fix deployment: use express server

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const port = 5000;
-
+const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, "dist")));
 
