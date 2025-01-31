0839adaf31085a8fbeb54a032b583460df0b8d4b
lipesc <felipedrhouse@hotmail.com>
2025-01-30 21:46:52 -0300

    update home script, style

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,4 +5,37 @@
   </div>
 </template>
 
-<script></script>
+<script>
+import { auth } from "../firebase";
+import { signOut } from "firebase/auth";
+
+export default {
+  setup() {
+    const logout = async () => {
+      try {
+        await signOut(auth);
+        alert("Logged out successfully.");
+      } catch (error) {
+        alert(error.message);
+      }
+
+    };
+
+    return { logout };
+  },
+};
+</script>
+
+<style scoped>
+.home-page {
+  text-align: center;
+  margin-top: 50px;
+
+
+}
+
+button {
+  padding: 10px;
+  margin-top: 20px;
+}
+</style>
